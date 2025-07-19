180c2294ca5dd1727df0a11231d9bcbd9875978e
janglez22 <allenbeau78@gmail.com>
2025-07-19 12:36:21 -0400

    Final Draft

---
+++ port.js
@@ -0,0 +1,32 @@
+const htmlSkills = document.getElementById('HTMLSkills');
+const cssSkills = document.getElementById('CSSSkills'); 
+const jsSkills = document.getElementById('JavaScriptSkills');
+const reactSkills = document.getElementById('ReactSkills');
+const nodeSkills = document.getElementById('NodeSkills');
+
+htmlSkills.addEventListener('click', () => {
+    htmlSkills.style.backgroundColor = 'white';
+    htmlSkills.style.color = 'black';
+    htmlSkills.style.fontSize = '3rem';
+});
+cssSkills.addEventListener('click', () => {
+    cssSkills.style.backgroundColor = 'white';
+    cssSkills.style.color = 'black';
+    cssSkills.style.fontSize = '3rem';
+});
+jsSkills.addEventListener('click', () => {
+    jsSkills.style.backgroundColor = 'white';
+    jsSkills.style.color = 'black';
+    jsSkills.style.fontSize = '3rem';
+});
+reactSkills.addEventListener('click', () => {
+    reactSkills.style.backgroundColor = 'white';
+    reactSkills.style.color = 'black';
+    reactSkills.style.fontSize = '3rem';
+});
+nodeSkills.addEventListener('click', () => {
+    nodeSkills.style.backgroundColor = 'white';
+    nodeSkills.style.color = 'black';
+    nodeSkills.style.fontSize = '3rem';
+});
+
